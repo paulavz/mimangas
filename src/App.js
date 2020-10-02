2a02f9a49421b5b586897f42cc2253feba7a0c6d
Paula Suarez <paula.sua.008@gmail.com>
2020-10-02 15:12:53 -0400

    quitando rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import Formulario from './Rutas/Formularios';
 import Principal from './Rutas/Principal';
 
@@ -13,29 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Router>
-          <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Principal</Link>
-              </li>
-              <li>
-                <Link to="/Login">Login</Link>
-              </li>
-            </ul>
-          </nav>
 
-          <Switch>
-            <Route path="/Login">
-              <Formulario />
-            </Route>
-            <Route path="/">
-              <Principal />
-            </Route>
-          </Switch>
-        </div>
-        </Router>
       </header>
     </div>
   );
