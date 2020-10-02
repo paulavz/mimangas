7462300ada935df23939c2db4bde897631990656
Paula Suarez <paula.sua.008@gmail.com>
2020-10-01 22:13:15 -0400

    firestore persistance

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import firebase from "./Inicializer/firebase";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+require("firebase/firebase-firestore");
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +13,21 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+firebase
+  .firestore()
+  .enablePersistence()
+  .catch(function (err) {
+    if (err.code === "failed-precondition") {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code === "unimplemented") {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
