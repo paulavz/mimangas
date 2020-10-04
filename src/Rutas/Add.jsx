ddb509eeb9e28176470428920d4919d8484e6a5d
Paula Suarez <paula.sua.008@gmail.com>
2020-10-04 16:29:21 -0400

    add icon

--- src/Rutas/Add.jsx
+++ src/Rutas/Add.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 export default function Add(props) {
 
     return <div>
         <Icon color="secondary">add_circle</Icon>
+
+        <IconButton aria-label="delete" >
+            <AddIcon fontSize="large" />
+        </IconButton>
+
     </div>
 
 
