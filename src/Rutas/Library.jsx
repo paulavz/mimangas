67c6d5c4518864336833519ee954f5a12c9361cb
Paula Suarez <paula.sua.008@gmail.com>
2020-10-04 15:25:52 -0400

    Base de Library

--- src/Rutas/Library.jsx
+++ src/Rutas/Library.jsx
@@ -1,8 +1,75 @@
 import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import firebase from "../Inicializer/firebase";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Divider from '@material-ui/core/Divider';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import "./Library.css";
 require("firebase/auth");
 
+const CssTextField = withStyles({
+    root: {
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'blue',
+            },
+        },
+    },
+})(TextField);
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+    },
+    form: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+    gridd: {
+        flexGrow: 1,
+        marginTop: theme.spacing(4),
+        margin: theme.spacing(1),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    divider: {
+        marginTop: theme.spacing(2),
+    },
+    colbutton: {
+        marginTop: theme.spacing(1),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textTransform: 'none',
+
+        '& > *': {
+            margin: theme.spacing(1),
+
+        },
+    },
+}));
+
+
+
+
 export default function Library(props) {
+    const classes = useStyles();
+
     function cerrar() {
         firebase
             .auth()
@@ -14,12 +81,57 @@ export default function Library(props) {
                 console.log("No se ha cerrado sección ", error);
             });
     }
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Mis Mangas
+          </Typography>
+                    <IconButton edge="start" onClick={cerrar} className={classes.menuButton} color="inherit" aria-label="menu">
+                        <ExitToAppIcon />
+                    </IconButton>
 
-    return <div>
-        <p>Ruta 1 compa</p>
-        <button onClick={cerrar}>Cerrar</button>
+                </Toolbar>
+
+            </AppBar>
+
+            <div className={classes.gridd}>
+                <Grid container spacing={2}>
+                    <Grid item xs={9}>
+                        <CssTextField
+                            fullWidth
+                            label="Buscar Manga"
+                            variant="outlined"
+                            id="outlined-search"
+                            type="search"
+                        />                    </Grid>
+                    <Grid item xs={3}>
+                        <Button size="large" className="buscar" fullWidth variant="contained" color="secondary">
+                            Búsqueda Avanzada
+</Button>
+                    </Grid>
+
+                </Grid>
+
+                <Divider className={classes.divider} variant="middle" />
+                <div className={classes.colbutton}>
+                    <ButtonGroup size="large" color="primary" aria-label="large outlined primary button group">
 
 
-    </div>
+                        <Button>Todos</Button>
+                        <Button>Siguiendo</Button>
+                        <Button>Completos</Button>
+                        <Button>Favoritos</Button>
+                        <Button>Pausados</Button>
+                        <Button>Pedientes</Button>
+                        <Button>Abandonados</Button>
+                    </ButtonGroup>
+                </div>
+
+            </div>
+        </div>
+    )
+
 
 }
