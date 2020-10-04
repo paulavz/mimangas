814062aad75d3377e1ee9296aa1c6cdce796fbc3
Paula Suarez <paula.sua.008@gmail.com>
2020-10-04 17:49:03 -0400

    addDialog

--- src/Rutas/Add.jsx
+++ src/Rutas/Add.jsx
@@ -4,6 +4,14 @@ import { makeStyles } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
 import { pink } from '@material-ui/core/colors';
 import Avatar from '@material-ui/core/Avatar';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import './Add.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -15,21 +23,54 @@ const useStyles = makeStyles((theme) => ({
     },
 
 }));
+
+
+
 export default function Add(props) {
-
-
-
     const classes = useStyles();
 
+    const [open, setOpen] = React.useState(false);
 
-    return <div className="add">
-        <IconButton size="small">
-            <Avatar className={classes.pink}>
-                <AddIcon />
-            </Avatar>
-        </IconButton>
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-
+    const handleClose = () => {
+        setOpen(false);
+    };
+    return <div>
+        <div className="add">
+            <IconButton size="small" onClick={handleClickOpen}>
+                <Avatar className={classes.pink}>
+                    <AddIcon />
+                </Avatar>
+            </IconButton>
+        </div>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    To subscribe to this website, please enter your email address here. We will send updates
+                    occasionally.
+          </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Cancel
+          </Button>
+                <Button onClick={handleClose} color="primary">
+                    Subscribe
+          </Button>
+            </DialogActions>
+        </Dialog>
     </div>
 
 
