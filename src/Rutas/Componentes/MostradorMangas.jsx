91da296c7b0249397a8f82155277d1a9a62bcca5
angeljsb <suangel.007@gmail.com>
2020-10-15 16:13:40 -0400

    MostradorMangas - Paginación Responsible

--- src/Rutas/Componentes/MostradorMangas.jsx
+++ src/Rutas/Componentes/MostradorMangas.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import TarjetaManga from './TarjetaManga';
 import Grid from '@material-ui/core/Grid';
 import TablePagination from '@material-ui/core/TablePagination';
+import Hidden from '@material-ui/core/Hidden';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+    paginationPC: {
+        [theme.breakpoints.down('xs')]: {
+            display: "none",
+        },
+    },
+}));
 
 export default function ({ mangas, itemsPorFila, initialRPP, paginationProps, ...otrasProps }) {
+
+    const classes = useStyles();
 
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -24,10 +37,16 @@ export default function ({ mangas, itemsPorFila, initialRPP, paginationProps, ..
         setPage(newPage);
     };
 
+    const handleChangePageMovil = (event, value) => {
+        setPage(value-1);
+    };
+
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
+
+    const {className, ...otrasPagProps} = paginationProps;
 
     return <div style={{ height: "100%", width: "100%" }}>
         <Grid container {...otrasProps} >
@@ -45,19 +64,30 @@ export default function ({ mangas, itemsPorFila, initialRPP, paginationProps, ..
             }
         </Grid>
 
-        {mangas.length > rowsPerPage && <TablePagination
-            component="div"
-            count={mangas.length}
-            page={page}
-            onChangePage={handleChangePage}
-            rowsPerPage={rowsPerPage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-            labelRowsPerPage="Mangas por pagina"
-            labelDisplayedRows={({ from, to, count }) => `${from} - ${to} de ${count}`}
-            nextIconButtonText="Siguiente"
-            backIconButtonText="Anterior"
-            {...paginationProps}
-        />}
+        {mangas.length > rowsPerPage && <React.Fragment>
+            <TablePagination
+                component="div"
+                count={mangas.length}
+                page={page}
+                onChangePage={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                labelRowsPerPage="Mangas por pagina"
+                labelDisplayedRows={({ from, to, count }) => `${from} - ${to} de ${count}`}
+                nextIconButtonText="Siguiente"
+                backIconButtonText="Anterior"
+                className={ [className || "", classes.paginationPC].join(" ") }
+                {...otrasPagProps}
+            />
+            <Hidden smUp >
+                <Pagination 
+                    count={Math.ceil(mangas.length/rowsPerPage)} 
+                    page={page+1}
+                    onChange={handleChangePageMovil}
+                    siblingCount={0}
+                />
+            </Hidden>
+        </React.Fragment>}
 
     </div>
 }
