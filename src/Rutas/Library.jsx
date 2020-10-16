262611fbdf2f5c8d877e2fd7bc964ac1c0d584ef
Paula Suarez <paula.sua.008@gmail.com>
2020-10-15 21:08:31 -0400

    Revirtiendo cambio de prueba

--- src/Rutas/Library.jsx
+++ src/Rutas/Library.jsx
@@ -125,7 +125,6 @@ export default function Library(props) {
                     </Toolbar>
 
                 </AppBar>
-                <h1>Hola Mundo</h1>
                 <div className={classes.gridd}>
 
                     {!cargando &&
